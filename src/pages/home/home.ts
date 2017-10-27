d6413b51385ba495010acc2d3d9a55bb59db7034
Enzo Cornand <goulaheau@gmail.com>
2017-10-27 14:34:13 +0200

    GET /pizza

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  pizzas: Array<any>;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,
+              private http: HttpClient) {
+    this.http.get('http://10.13.2.167:8080/pizza').subscribe(
+      (res: any) => this.pizzas = res,
+      err => console.log(err)
+    );
   }
 
 }
